85aad2b8388674f4b51edcbfdbc5066bf4adc84d
mary-okinawa <iscreamoe88@gmail.com>
2020-03-04 17:44:33 +0900

    時間管理ライブラリのお勉強！

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import MyPage from './components/MyPage';
 import TopPage from './components/TopPage';
+import Time from './components/Time';
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
     <Router>
       <Route exact path="/" component = {TopPage} />
       <Route exact path="/mypage" component = {MyPage} />
+      <Route exact path="/time" component = {Time} />
     </Router>
   );
 }

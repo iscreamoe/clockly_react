260d03f40d4c9ea9746d429ff76c48d8290e0c96
mary-okinawa <iscreamoe88@gmail.com>
2020-03-06 17:31:37 +0900

    modal 消したー

--- src/components/TopPage.js
+++ src/components/TopPage.js
@@ -1,47 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import { Header, Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
 import bgImg from '../images/bgImg.jpg';
 
-const Modal = ({ hideModal }) => {
-  return (
-    <ModalMask>
-      <ModalContents>
-      <form action="#">
-          <div>
-            <label for="name">Name</label>
-            <input type="text" id="name" name="yourname" />
-          </div>
-
-          <div>
-            <label for="email">E-mail</label>
-            <input type="email" id="email" name="your-email" />
-          </div>
-
-          <div>
-            <label for="message">お名前</label>
-            <textarea id="message" name="your-message"></textarea>
-          </div>
-
-            <input type="submit" class="button" value="Sign Up" /> 
-          <ModalClose onClick={hideModal}>
-            Sign Up
-          </ModalClose>
-        </form>
-        
-      </ModalContents>
-    </ModalMask>
-  );
-};
-
 const TopPage = () => {
-  const [isShow, setIsShow] = useState(false);
-  const showModal = () => setIsShow(true);
-  const hideModal = () => setIsShow(false);
   return (
     <>
-      {isShow && <Modal hideModal={hideModal} />}
       <Wrapper>
         <Header as='h2' icon textAlign='center' className="top_header">
           <Logo>
@@ -50,7 +15,7 @@ const TopPage = () => {
           </Logo>
         </Header>
         <Container>
-          <Button onClick={showModal}>Sign up</Button>
+          <Button>Sign up</Button>
           <Button><Link to='/MyPage'>Log in</Link></Button>
         </Container>
       </Wrapper>
@@ -85,36 +50,5 @@ const Button = styled.button`
   padding: 16px 24px;
   letter-spacing: 1px;
 `
-const ModalMask = styled.div`
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(45, 45, 45, 0.54);
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-const ModalContents = styled.div`
-  position: relative;
-  min-width: 500px;
-  min-height: 450px;
-
-  padding: 2rem;
-
-  border-radius: 50px;
-  background: #fff;
-
-  text-align: center;
-`
-const ModalClose = styled.button`
-  background: transparent;
-  border-radius: 100px / 90px;
-  border: 2px solid palevioletred;
-  color: gray;
-  margin: 16px auto;
-  padding: 8px 24px;
-  letter-spacing: 1px;
-  display: block;
-`
 export default TopPage;

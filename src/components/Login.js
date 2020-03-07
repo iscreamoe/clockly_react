91dd1dd631b82e1e5200cb58dc5a2b366bb6b194
mary-okinawa <iscreamoe88@gmail.com>
2020-03-08 01:01:37 +0900

    Loginページできた

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+
 import styled from 'styled-components';
 import loginBg from '../images/loginBg.jpg';
 
 const Login = () => {
   return (
     <>
-    <App>
-    <Card>
-    <Title>Login</Title>
-        <form>
-          <p>Email</p>
-          <input type="email" />
-          <p>password</p>
-          <input type="password" />
-          <Link to='/mypage'>
-            <input type="submit" value="LOGIN" />
-          </Link>
-        </form>
-    </Card>
-    </App>
+      <Wrapper>
+        <Card>
+          <Container>
+            <Link to='/'><Close>Close</Close></Link>
+            <Title>LOGIN</Title>
+              <form action="#">
+                <Label htmlFor="email">
+                  Email
+                </Label>
+                <Input>
+                  <input type="email" name="email" id="email" />
+                </Input>
+
+                <Label htmlFor="password">
+                  password
+                </Label>
+                <Input>
+                  <input type="password" name="password" id="password" />
+                </Input>
+
+                <Link to='/mypage'>
+                  <Button>LOGIN</Button>
+                </Link>
+              </form>
+          </Container>
+        </Card>
+      </Wrapper>
     </>
   );
 }
 
-const Title = styled.div`
-  color: #333;
-  font-size: 32px;
-  letter-spacing: 1.5px;
-  margin: 32px auto;
-  text-align: center;
-`
-const App = styled.div`
+const Wrapper = styled.div`
   background: #eef5ee;
   min-height: 100vh;
   display: flex;
@@ -42,9 +49,27 @@ const App = styled.div`
   background-size: cover;
   height: 100vh;
 `
+const Close =styled.div`
+  position: absolute;
+  right: 40rem;
+  color: #333;
+  font-size: 16px;
+`
+const Title = styled.div`
+  color: #333;
+  font-size: 32px;
+  letter-spacing: 1.5px;
+  margin: 32px auto;
+  text-align: center;
+`
+const Container = styled.div`
+  margin-top: 50px;
+  text-align: center;
+  margin: 0 auto 10px;
+`
 const Card = styled.div`
   min-width: 400px;
-  min-height: 400px;
+  min-height: 450px;
 
   justify-content: center;
   align-items: center;
@@ -52,6 +77,36 @@ const Card = styled.div`
   border-radius: 50px;
   background: #eef5ee;
   box-shadow: 20px 20px 60px #cad0ca, -20px -20px 60px #ffffff;
+`
+const Label = styled.div`
+  font-size: 16px;
+  margin-bottom: 10px;
+  display: block;
+`
+const Input = styled.div`
+  border-radius: 5px;
+  padding:10px;
+  font-size:16px;
+`
+const Button = styled.button`
+  width: 120px;
+  height: 50px;
+  font-size: 14px;
+  background: transparent;
+  border-radius: 40px;
+  border: 2px solid palevioletred;
+  color: #333;
+  margin: 16px auto 0;
+  padding: 16px 24px;
+  letter-spacing: 1.5px;
+  display: block;
+  &:focus {
+    outline:0
+  };
+  &:hover {
+    background-color: pink;
+    border: none;
+  };
 `
 
 export default Login;

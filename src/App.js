d9c2c7a63ec369946760f1fcddf5e4c4402e5d29
mary-okinawa <iscreamoe88@gmail.com>
2020-03-09 16:02:05 +0900

    css直した

--- src/App.js
+++ src/App.js
@@ -6,40 +6,26 @@ import {
   Switch
 } from "react-router-dom";
 
-import { Header, Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
 import bgImg from './images/bgImg.jpg';
 
 import SignUp from './components/SignUp';
 import Login from './components/Login';
-// import MyPage from './components/MyPage';
-// import Profile from './components/Profile';
+import MyPage from './components/MyPage';
+import Profile from './components/Profile';
+import Home from './components/Home';
 
 const App = () => {
   return (
     <Router>
       <Wrapper>
         <Switch>
+          <Route exact path="/" component = {Home} />
+          <Route exact path="/mypage" component = {MyPage} />
+          <Route exact path="/profile" component = {Profile} />
           <Route exact path="/signup" component = {SignUp} />
           <Route exact path="/login" component = {Login} />
-          {/* <Route exact path="/mypage" component = {MyPage} />
-          <Route exact path="/profile" component = {Profile} /> */}
         </Switch>
-
-        <Header as='h2' icon textAlign='center' className="top_header">
-          <Logo>
-            <Icon name='clock outline' />
-            <Title>CLOCKLY</Title>
-          </Logo>
-        </Header>
-        <Container>
-          <Link to='/signup'>
-            <Button>SIGNUP</Button>
-          </Link>
-          <Link to='/login'>
-            <Button>LOGIN</Button>
-          </Link>
-        </Container>
       </Wrapper>
     </Router>
   );
@@ -50,39 +36,5 @@ const Wrapper = styled.div`
   background-size: cover;
   height: 100vh;
 ` 
-const Logo = styled.div`
-  padding-top: 32px;
-  color: #fff;
-` 
-const Title = styled.div`
-  font-size: 32px;
-  letter-spacing: 1.5px;
-  margin-top: 32px;
-`
-const Container = styled.div`
-  margin-top: 50px;
-  text-align: center;
-  color: #fff;
-`
-const Button = styled.button`
-  width: 150px;
-  height: 60px;
-  font-size: 16px;
-  background: transparent;
-  border-radius: 40px;
-  border: 2px solid palevioletred;
-  color: #fff;
-  margin: 24px;
-  padding: 16px 24px;
-  letter-spacing: 1.5px;
-  display: inline-block;
-  &:focus {
-    outline:0
-  };
-  &:hover {
-    background-color: rgb(197, 171, 172);
-    border: none;
-  };
-`
 
 export default App;
